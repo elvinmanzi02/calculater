9120131a852aeaaa0faa37e3b446eac6bbe86acb
elvinmanzi02 <elvinmanzi@gmail.com>
2023-07-18 13:18:19 +0200

    almost html and css well done, except javascript

---
+++ script.js
@@ -0,0 +1,7 @@
+// Get the height and width of the screen in pixels
+const screenHeight = window.screen.height;
+const screenWidth = window.screen.width;
+
+// Log the values to the console
+console.log("Screen height (pixels):", screenHeight);
+console.log("Screen width (pixels):", screenWidth);
